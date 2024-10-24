208b26a7fd61779e265895e62b7bb39b01f8a651
Akash Desai <gpkp176172@gmail.com>
2024-10-24 19:58:49 +0530

    Created GraphQL APIs

--- server/index.js
+++ server/index.js
@@ -9,19 +9,38 @@ async function startServer() {
     const app = express();
     const server = new ApolloServer({
         typeDefs: `
+        type User {
+            id: ID!,
+            username: String,
+            firstName: String,
+            lastName: String,
+            email: String
+        }
+
         type Todo {
             id: ID!,
             todo: String,
-            completed: Boolean
-            userId: ID
+            completed: Boolean,
+            user: User
         }
+
         type Query {
             getTodos: [Todo]
+            getAllUsers: [User]
+            getUser(id:ID!): User 
         }
         `,
         resolvers: {
+            Todo: {
+                user: async (todo) => {
+                    return await (await axios.get(`https://dummyjson.com/users/${todo.id}`)).data;
+                }
+            },
+
             Query: {
-                getTodos: async () => (await axios.get('https://dummyjson.com/todos/random/10')).data
+                getTodos: async () => (await axios.get('https://dummyjson.com/todos/random/10')).data, // dummy API urls for getting dummy data
+                getAllUsers: async () => (await axios.get('https://dummyjson.com/users')).data.users,
+                getUser: async (parent, { id }) => (await axios.get(`https://dummyjson.com/users/${id}`)).data
             }
         }
     });
